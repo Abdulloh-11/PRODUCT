4329e27619b6daae75b88b0b96b65d6b7c4c508c
Abdulloh-11 <abdullohgulomov1234@gmail.com>
2022-11-06 21:04:38 +0500

    first commit

--- pricing_table/script.js
+++ pricing_table/script.js
@@ -4,10 +4,3 @@ let modeBtn = document.querySelector(".mode-btn");
 modeBtn.addEventListener('click', function(){
     body.classList.toggle('body-dark')
 })
-
-let bar = document.querySelector("bar");
-let burger = document.querySelector(".burger")
-
-burger.addEventListener('click', function(){
-    bar.classList.toggle('bar')
-})
